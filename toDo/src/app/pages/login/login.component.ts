fcc03265d2b27cfccaf569dfb3c258acaa9c10a5
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-01-29 01:35:44 +0200

    login+todo

--- toDo/src/app/pages/login/login.component.ts
+++ toDo/src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!:FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm=this.fb.group({
+      email:new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required]),
+
+    });
+  }
+  onSubmit(){
+    if(this.loginForm.valid){
+
+    }else{
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
--- toDo/src/app/pages/todo/todo.component.ts
+++ toDo/src/app/pages/todo/todo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TodoCardComponent } from '../../shared/components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [TodoCardComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
