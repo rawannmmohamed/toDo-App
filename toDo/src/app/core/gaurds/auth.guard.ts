a5a7c388d59f09b9ff3903bdbcccf83097b19602
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-01-29 19:24:11 +0200

    gaurds

--- toDo/src/app/core/gaurds/auth.guard.ts
+++ toDo/src/app/core/gaurds/auth.guard.ts
@@ -1,5 +1,12 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '../services/token.service';
+import { inject } from '@angular/core';
+
 
 export const authGuard: CanActivateFn = (route, state) => {
+
+   const  token = inject(TokenService);
+   const router = inject(Router);
+
   return true;
 };
